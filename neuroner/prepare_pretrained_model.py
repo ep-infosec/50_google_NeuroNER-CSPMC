a9dff5deb000e62003ba54f576f4a5f9ea4e69b0
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-13 14:55:28 +0200

    init

---
+++ neuroner/prepare_pretrained_model.py
@@ -0,0 +1,188 @@
+# MIT License
+#
+# Copyright 2019 Google LLC
+# Copyright (c) 2019 Franck Dernoncourt, Jenny Lee, Tom Pollard
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+This script prepares a pretrained model to be shared without exposing the data used for training.
+'''
+import glob
+import os
+import pickle
+from pprint import pprint
+import shutil
+
+from neuroner import utils
+from neuroner.entity_lstm import EntityLSTM
+import tensorflow as tf
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+
+from neuroner import utils_tf
+from neuroner import neuromodel
+
+def trim_dataset_pickle(input_dataset_filepath, output_dataset_filepath=None, delete_token_mappings=False, verbose=False):
+    '''
+    Remove the dataset and labels from dataset.pickle. 
+    If delete_token_mappings = True, then also remove token_to_index and index_to_token except for UNK.
+    '''
+    print("Trimming dataset.pickle..")
+    if output_dataset_filepath == None:
+        output_dataset_filepath = os.path.join(os.path.dirname(input_dataset_filepath), 
+            'dataset_trimmed.pickle')
+    dataset = pickle.load(open(input_dataset_filepath, 'rb'))
+    count = 0
+    print("Keys removed:")
+    keys_to_remove = ['character_indices', 'character_indices_padded', 'characters', 
+        'label_indices', 'label_vector_indices', 'labels', 'token_indices', 
+        'token_lengths', 'tokens', 'infrequent_token_indices', 'tokens_mapped_to_unk']
+    for key in keys_to_remove:
+        if key in dataset.__dict__:
+            del dataset.__dict__[key]
+            print('\t' + key)
+            count += 1            
+    if delete_token_mappings:
+        dataset.__dict__['token_to_index'] = {dataset.__dict__['UNK']:dataset.__dict__['UNK_TOKEN_INDEX']}
+        dataset.__dict__['index_to_token'] = {dataset.__dict__['UNK_TOKEN_INDEX']:dataset.__dict__['UNK']}
+    print("Number of keys removed: {0}".format(count))
+    if verbose:
+        pprint(dataset.__dict__)
+    pickle.dump(dataset, open(output_dataset_filepath, 'wb'))
+    print("Done!")
+
+
+def trim_model_checkpoint(parameters_filepath, dataset_filepath, input_checkpoint_filepath, 
+                          output_checkpoint_filepath, verbose=False):
+    '''
+    Remove all token embeddings except UNK.
+    '''
+    parameters, _ = neuromodel.load_parameters(parameters_filepath=parameters_filepath)
+    dataset = pickle.load(open(dataset_filepath, 'rb'))
+    model = EntityLSTM(dataset, parameters) 
+    with tf.Session() as sess:
+        model_saver = tf.train.Saver()  # defaults to saving all variables
+        
+        # Restore the pretrained model
+        model_saver.restore(sess, input_checkpoint_filepath) # Works only when the dimensions of tensor variables are matched.
+        
+        # Get pretrained embeddings
+        token_embedding_weights = sess.run(model.token_embedding_weights) 
+    
+        # Restore the sizes of token embedding weights
+        utils_tf.resize_tensor_variable(sess, model.token_embedding_weights, 
+            [1, parameters['token_embedding_dimension']]) 
+            
+        initial_weights = sess.run(model.token_embedding_weights)
+        initial_weights[dataset.UNK_TOKEN_INDEX] = token_embedding_weights[dataset.UNK_TOKEN_INDEX]
+        sess.run(tf.assign(model.token_embedding_weights, initial_weights, validate_shape=False))
+    
+        token_embedding_weights = sess.run(model.token_embedding_weights) 
+        if verbose:
+            print("token_embedding_weights: {0}".format(token_embedding_weights))
+        
+        model_saver.save(sess, output_checkpoint_filepath)
+            
+    dataset.__dict__['vocabulary_size'] = 1
+    pickle.dump(dataset, open(dataset_filepath, 'wb'))
+    if verbose:
+        pprint(dataset.__dict__)
+
+
+def prepare_pretrained_model_for_restoring(output_folder_name, epoch_number, 
+                                           model_name, delete_token_mappings=False, verbose=False):
+    '''
+    Copy the dataset.pickle, parameters.ini, and model checkpoint files after 
+    removing the data used for training.
+    
+    The dataset and labels are deleted from dataset.pickle by default. The only 
+    information about the dataset that remain in the pretrained model
+    is the list of tokens that appears in the dataset and the corresponding token 
+    embeddings learned from the dataset.
+    
+    If delete_token_mappings is set to True, index_to_token and token_to_index 
+    mappings are deleted from dataset.pickle additionally,
+    and the corresponding token embeddings are deleted from the model checkpoint 
+    files. In this case, the pretrained model would not contain
+    any information about the dataset used for training the model. 
+    
+    If you wish to share a pretrained model with delete_token_mappings = True, 
+    it is highly recommended to use some external pre-trained token 
+    embeddings and freeze them while training the model to obtain high performance. 
+    This can be done by specifying the token_pretrained_embedding_filepath 
+    and setting freeze_token_embeddings = True in parameters.ini for training.
+    '''
+    input_model_folder = os.path.join('.', 'output', output_folder_name, 'model')
+    output_model_folder = os.path.join('.', 'trained_models', model_name)
+    utils.create_folder_if_not_exists(output_model_folder)
+
+    # trim and copy dataset.pickle
+    input_dataset_filepath = os.path.join(input_model_folder, 'dataset.pickle')
+    output_dataset_filepath = os.path.join(output_model_folder, 'dataset.pickle')
+    trim_dataset_pickle(input_dataset_filepath, output_dataset_filepath, 
+                        delete_token_mappings=delete_token_mappings,verbose=verbose)
+    
+    # copy parameters.ini
+    parameters_filepath = os.path.join(input_model_folder, 'parameters.ini')
+    shutil.copy(parameters_filepath, output_model_folder)
+    
+    # (trim and) copy checkpoint files
+    epoch_number_string = str(epoch_number).zfill(5)
+    if delete_token_mappings:
+        input_checkpoint_filepath = os.path.join(input_model_folder, 
+            'model_{0}.ckpt'.format(epoch_number_string))
+        output_checkpoint_filepath = os.path.join(output_model_folder, 'model.ckpt')
+        trim_model_checkpoint(parameters_filepath, output_dataset_filepath, 
+                              input_checkpoint_filepath, output_checkpoint_filepath, verbose)
+    else:
+        for filepath in glob.glob(os.path.join(input_model_folder, 
+            'model_{0}.ckpt*'.format(epoch_number_string))):
+            shutil.copyfile(filepath, os.path.join(output_model_folder, 
+                os.path.basename(filepath).replace('_' + epoch_number_string, '')))
+
+ 
+def check_contents_of_dataset_and_model_checkpoint(model_folder):
+    '''
+    Check the contents of dataset.pickle and model_xxx.ckpt.
+    model_folder: folder containing dataset.pickle and model_xxx.ckpt to be checked. 
+    '''
+    dataset_filepath = os.path.join(model_folder, 'dataset.pickle')
+    dataset = pickle.load(open(dataset_filepath, 'rb'))
+    pprint(dataset.__dict__)
+    pprint(list(dataset.__dict__.keys()))
+
+    checkpoint_filepath = os.path.join(model_folder, 'model.ckpt')
+    with tf.Session() as sess:
+        print_tensors_in_checkpoint_file(checkpoint_filepath, 
+            tensor_name='token_embedding/token_embedding_weights', all_tensors=True)
+        print_tensors_in_checkpoint_file(checkpoint_filepath, 
+            tensor_name='token_embedding/token_embedding_weights', all_tensors=False)
+
+
+if __name__ == '__main__':
+    output_folder_name = 'en_2017-05-05_08-58-32-633799'
+    epoch_number = 30
+    model_name = 'conll_2003_en'
+    delete_token_mappings = False
+    prepare_pretrained_model_for_restoring(output_folder_name, epoch_number, 
+        model_name, delete_token_mappings)
+    
+#     model_name = 'mimic_glove_spacy_iobes'
+#     model_folder = os.path.join('..', 'trained_models', model_name)
+#     check_contents_of_dataset_and_model_checkpoint(model_folder)
